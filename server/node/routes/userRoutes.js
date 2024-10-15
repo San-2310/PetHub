e052209f1011f65b9b6a95c08aa5745073a70b04
Saachi <peswanisaachi@gmail.com>
2024-10-15 21:21:01 +0530

    models added

--- server/node/routes/userRoutes.js
+++ server/node/routes/userRoutes.js
@@ -1,9 +1,16 @@
 import express from 'express';
-import { saveOrUpdateUser, getUser } from '../controllers/user.controller.js';
+import { 
+  saveOrUpdateUser, 
+  getUser, 
+  addPetToUser, 
+  removePetFromUser 
+} from '../controllers/user.controller.js';
 
 const router = express.Router();
 
 router.post('/saveUserData', saveOrUpdateUser);
 router.get('/:id', getUser);
+router.post('/:id/pets', addPetToUser);
+router.delete('/:userId/pets/:petId', removePetFromUser);
 
 export default router;
